82347c5481239744ebcefab68676fce0a5de0d3f
jeevisaminathan <jeevitha.saminathan13@gmail.com>
2023-12-08 19:55:02 -0600

    no changes

---
+++ PythonPrograms/stringconversion.py
@@ -0,0 +1,7 @@
+user_input=input("Enter the string: ")
+if user_input.isupper():
+    user_input=user_input.lower()
+    print(user_input)
+else:
+    user_input=user_input.upper()
+    print(user_input)
